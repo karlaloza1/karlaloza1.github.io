845e3fdb1f19f5375be602d861707f244e5e5196
Karla <domenica.lozada97@gmail.com>
2020-08-29 19:39:33 -0700

    c

--- resume 2/contact.php
+++ resume 2/contact.php
@@ -1,21 +1,22 @@
 <?php
+
+if(isset($_POST['submit']))
+{
     $name = $_POST['name'];
-    $visitor_email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $mailFrom = $_POST['mail'];
     $message = $_POST['message'];
 
 
+    $mailTo = "domenilozada@hotmail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
-    $email_form = 'domenica.lozada97@gmail.com';
-    $email_subject = "New Form Submission";
 
-    $email_body = "User Name: $name.\n".
-                    "User Email: $visitor_email.\n".
-                        "User Message: $message.\n";
+    mail($mailTo, $subject, $txt, $headers);
 
-    $to = "domenica.lozada97@gmail.com";
-    $headers = "From: $email_form\r\n";
-    $headers .= "Reply-To: $visitor_email \r\n";
-    mail($to, $email_subject, $email_body, $headers);
-    header("Location: index.html");
+    header("Location: index.html?mailsend");
+}
+    
 
 ?>
